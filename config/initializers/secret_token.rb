366a75dc69135a2d825b8e9d5765ed4b49a027cc
Shaq Isaak <shaq0001@gmail.com>
2012-10-14 10:29:52 -0500

    initi commint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FFBuzz::Application.config.secret_token = 'ce3a187ba8fde4d44445c55f0ede4748c2607da9be9ccbfe8f6ff1dc7864801be7069e870b7ae63416b0d41940af7a96b02c0e2aa2332cc3da22c6ecc4c792c9'
